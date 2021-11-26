8df80e85443c237e1834770062d2c08a0812924e
Aaron Meaney <aaronmeaney@hotmail.com>
2021-11-26 10:22:30 +0000

    Implement ScannerBot patrol

--- Assets/Game/Core/Characters/ScannerBot.cs
+++ Assets/Game/Core/Characters/ScannerBot.cs
@@ -11,23 +11,23 @@ public class ScannerBot : MonoBehaviour
     private Rigidbody2D m_rb;
     private BotAI m_AI;
 
-    private List<Vector2Int> m_path;
-    private int m_pathIdx = 0;
+    private List<Vector2Int> m_patrolRoute;
+    private int m_patrolIdx = 0;
 
-    private Vector2Int DestinationCellPosition => m_path[m_pathIdx];
+    private Vector2Int DestinationCellPosition => m_patrolRoute[m_patrolIdx];
     private Vector3 DestinationWorldPosition => m_levelManager.WorldPosition((Vector3Int)DestinationCellPosition);
 
     private Vector3Int m_targetPosition;
 
     private LevelTile m_currentLevelTile;
 
+    private bool m_movePositive = true;
+
     private void Awake()
     {
         m_AI = FindObjectOfType<BotAI>();
         m_levelManager = FindObjectOfType<LevelManager>();
         m_rb = GetComponent<Rigidbody2D>();
-
-        //m_levelManager.OnLevelUpdate += Updat
     }
 
     private void Start()
@@ -37,73 +37,61 @@ public class ScannerBot : MonoBehaviour
 
     private void UpdatePath()
     {
-        m_path = m_AI.FindPatrolRouteForScanBot((Vector2Int)m_levelManager.CellPosition(transform.position));
+        m_patrolRoute = m_AI.FindPatrolRouteForScanBot((Vector2Int)m_levelManager.CellPosition(transform.position));
 
-        // Prevent backtracking on path when it updates
-        if (m_path.Count > 0)
+        // Set the patrolIdx as closest route
+        float closestDistance = float.MaxValue;
+
+        for (int i = 0; i < m_patrolRoute.Count; ++i)
         {
-            m_pathIdx = 1;
-        }
-        else
-        {
-            m_pathIdx = 0;
+            Vector2Int currentPosition = (Vector2Int)m_levelManager.CellPosition(transform.position);
+            float dist = Vector2Int.Distance(m_patrolRoute[i], currentPosition);
+
+            if (dist < closestDistance)
+            {
+                closestDistance = dist;
+                m_patrolIdx = i;
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        return;
-
         m_rb.velocity = (DestinationWorldPosition - transform.position).normalized * m_speed * Time.deltaTime;
 
         if (Vector3.Distance(DestinationWorldPosition, transform.position) < 0.2f)
         {
-            if (m_pathIdx < m_path.Count - 1)
-            {
-                m_pathIdx++;
-                m_currentLevelTile = m_levelManager.GetTileInfo(DestinationCellPosition);
-            }
+            if (m_patrolIdx == 0)
+                m_movePositive = true;
+
+            if (m_patrolIdx == m_patrolRoute.Count - 1)
+                m_movePositive = false;
+
+            if (m_movePositive)
+                m_patrolIdx++;
             else
-            {
-                m_rb.velocity = Vector2.zero;
-            }
+                m_patrolIdx--;
+
+            m_currentLevelTile = m_levelManager.GetTileInfo(DestinationCellPosition);
         }
     }
 
     private void Update()
     {
-        /*
-        for (int i = 1; i < m_path.Count; ++i)
+        for (int i = 1; i < m_patrolRoute.Count; ++i)
         {
-            var from = m_levelManager.WorldPosition((Vector3Int)m_path[i - 1]);
-            var to = m_levelManager.WorldPosition((Vector3Int)m_path[i]);
-            Debug2.DrawArrow(from, to, Color.green);
-        }
-
-        Debug2.DrawArrow(transform.position, transform.position + (Vector3)m_rb.velocity, Color.blue);
-        */
-
-        /*
-        foreach (var t in m_path)
-        {
-            Debug2.DrawCross(m_levelManager.WorldPosition((Vector3Int)t), Color.red);
-        }
-        */
-
-        for (int i = 1; i < m_path.Count; ++i)
-        {
-            Debug2.DrawArrow(m_levelManager.WorldPosition(m_path[i - 1]), m_levelManager.WorldPosition(m_path[i]), Color.green);
+            Debug2.DrawArrow(m_levelManager.WorldPosition(m_patrolRoute[i - 1]), m_levelManager.WorldPosition(m_patrolRoute[i]), Color.green);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            m_pathIdx--;
+            m_patrolIdx--;
             transform.position = DestinationWorldPosition;
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            m_pathIdx++;
+            m_patrolIdx++;
             transform.position = DestinationWorldPosition;
         }
     }
